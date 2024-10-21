2b87a32a67815cfc6261dece92be1b2de9166019
valie <isauravpr@gmail.com>
2024-10-21 10:05:14 -0600

    [ADD] sensores

--- lib/screens/sensor_screen.dart
+++ lib/screens/sensor_screen.dart
@@ -1,4 +1,3 @@
-// lib/screens/sensor_screen.dart
 import 'package:flutter/material.dart';
 import 'package:sensors_plus/sensors_plus.dart';
 
@@ -8,10 +7,20 @@ class SensorScreen extends StatefulWidget {
 }
 
 class _SensorScreenState extends State<SensorScreen> {
-  // Variables para almacenar datos del sensor
+  // Variables para almacenar datos del acelerómetro
   double _accelerometerX = 0.0;
   double _accelerometerY = 0.0;
   double _accelerometerZ = 0.0;
+
+  // Variables para almacenar datos del giroscopio
+  double _gyroscopeX = 0.0;
+  double _gyroscopeY = 0.0;
+  double _gyroscopeZ = 0.0;
+
+  // Variables para almacenar datos del magnetómetro
+  double _magnetometerX = 0.0;
+  double _magnetometerY = 0.0;
+  double _magnetometerZ = 0.0;
 
   @override
   void initState() {
@@ -25,20 +34,53 @@ class _SensorScreenState extends State<SensorScreen> {
         _accelerometerZ = event.z;
       });
     });
+
+    // Escuchar los cambios del giroscopio
+    gyroscopeEvents.listen((GyroscopeEvent event) {
+      setState(() {
+        _gyroscopeX = event.x;
+        _gyroscopeY = event.y;
+        _gyroscopeZ = event.z;
+      });
+    });
+
+    // Escuchar los cambios del magnetómetro
+    magnetometerEvents.listen((MagnetometerEvent event) {
+      setState(() {
+        _magnetometerX = event.x;
+        _magnetometerY = event.y;
+        _magnetometerZ = event.z;
+      });
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("Datos de los Sensores")),
-      body: Center(
+      body: SingleChildScrollView(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
+            // Datos del acelerómetro
             Text('Acelerómetro'),
             Text('X: $_accelerometerX'),
             Text('Y: $_accelerometerY'),
             Text('Z: $_accelerometerZ'),
+            SizedBox(height: 20),
+
+            // Datos del giroscopio
+            Text('Giroscopio'),
+            Text('X: $_gyroscopeX'),
+            Text('Y: $_gyroscopeY'),
+            Text('Z: $_gyroscopeZ'),
+            SizedBox(height: 20),
+
+            // Datos del magnetómetro
+            Text('Magnetómetro'),
+            Text('X: $_magnetometerX'),
+            Text('Y: $_magnetometerY'),
+            Text('Z: $_magnetometerZ'),
           ],
         ),
       ),
